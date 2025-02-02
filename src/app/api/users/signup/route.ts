0d44a59791ac05f7490b365fed18ac3493653ca6
PEEYUSH KUMAR <peeyush.buisness45@gmail.com>
2025-02-02 22:53:31 +0530

    add signup and login code

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,7 +2,6 @@ import { connect } from "@/db/db";
 import User from "@/models/userModel.js";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 
 connect();
 
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,46 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
 
-  const onLogin = async () => {};
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login error", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      // check if email and password are not empty
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Login</h1>
+      <h1>{loading ? "Processing" : "Login"}</h1>
       <br />
       <label htmlFor="email">email</label>
       <input
@@ -40,7 +66,7 @@ export default function LoginPage() {
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
         onClick={onLogin}
       >
-        Login
+        {buttonDisabled ? "No login" : "Login"}
       </button>
       <Link href="/signup">Visit Signup Page</Link>
     </div>
