a4395b8d41932052535ad23ecedeb3cf4d3db48c
PEEYUSH KUMAR <peeyush.buisness45@gmail.com>
2025-02-07 00:15:27 +0530

    add middleware logic to codebase

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,9 +3,12 @@
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("Hello World");
   const logout = async () => {
     try {
       await axios.get("/api/users/logout");
@@ -16,17 +19,37 @@ export default function ProfilePage() {
       toast.error(error.message);
     }
   };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <br />
       <p>You are logged in</p>
+      <h2>
+        {data === "Hello World" ? (
+          "Hello World"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <br />
       <button
         onClick={logout}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Logout
+      </button>
+
+      <button
+        onClick={getUserDetails}
+        className="bg-green-700 hover:bg-green-400 text-white font-bold py-2 px-4 rounded"
+      >
+        Get User
       </button>
     </div>
   );
